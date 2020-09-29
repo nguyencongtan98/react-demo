7ba31f4ebab2a47cb8b955e1662a4133ab09b175
Nguyen Cong Tan <tancoder01@gmail.com>
2020-09-29 21:49:35 +0700

    Updates

---
+++ precache-manifest.36aa0ca024ddb32989bd5ca12823dfed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9040c3587ca407f4dbe37b684b003e18",
+    "url": "/nguyencongtan98/react-demo/index.html"
+  },
+  {
+    "revision": "0c80323cea507508974a",
+    "url": "/nguyencongtan98/react-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20fe2c4577ec4136b732",
+    "url": "/nguyencongtan98/react-demo/static/js/2.1fb6f890.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nguyencongtan98/react-demo/static/js/2.1fb6f890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c80323cea507508974a",
+    "url": "/nguyencongtan98/react-demo/static/js/main.defc4660.chunk.js"
+  },
+  {
+    "revision": "2e935c7b8a6ed5bf5cf4",
+    "url": "/nguyencongtan98/react-demo/static/js/runtime-main.08828c99.js"
+  }
+]);
