31cbee319c74438882a09b0dd89c4fd9650b1d7c
Nguyen Cong Tan <tancoder01@gmail.com>
2020-09-29 22:45:48 +0700

    update file task1.tsx

---
+++ src/actions/task2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Taks = () => {
+  return (
+    <div>
+      <h1>Task2</h1>
+      <h1>Task2</h1>
+      <h1>Task2</h1>
+      <h1>Task2</h1>
+    </div>
+  );
+};
